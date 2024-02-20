98073a720daced31f79f6e75ab095980019726e7
sivasaianjur1412 <sivasaianjur1412@gmail.com>
2024-02-20 14:42:42 -0600

    sql

--- library.sql
+++ library.sql
@@ -1,12 +1,15 @@
 create database library;
 use library;
-create table members(memberId integer, firstName varchar(40),
+create table members(memberId integer primary key, firstName varchar(40),
 					lastName varchar(40), emailId varchar(40),
 					phoneNumber varchar(10));
 
-create table book(bookId integer, bookName varchar(40),
+create table book(bookId integer primary key, bookName varchar(40),
 					author varchar(40), edition varchar(40),
 					price varchar(10));
                     
+insert into members values(2, "Siva", "Sai", "siva@g.c", "9717461874");
+insert into book values(1, "Java Programming", "Shiva", "second", "24");
+
 select * from members;
 select * from book;
